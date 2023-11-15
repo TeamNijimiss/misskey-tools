c6ea233de4bb9861e27309f614646fe3d599b8e9
Nafu Satsuki <satsuki@nafusoft.dev>
2023-11-16 03:12:07 +0900

    無限ループを修正

--- src/backend/functions/gen-token.ts
+++ src/backend/functions/gen-token.ts
@@ -12,7 +12,7 @@ export const genToken = async (): Promise<string> => {
     // eslint-disable-next-line @typescript-eslint/ban-ts-comment
     // @ts-ignore
     token = rndstr(32);
-    used = await UsedTokens.findOneBy({ token: token });
-  } while (used !== undefined);
+    used = await UsedTokens.findOneBy({ token: token }); //todo 現時点でなんの意味も成してない
+  } while (used);
   return token;
 };
